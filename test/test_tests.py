7e437328cfac2dcd96ba153e6765b4154296170d
studybuffalo <studybuffalo@gmail.com>
2018-04-12 21:56:00 -0600

    Standardizing test directory name, ignoring coverage and pytest files

---
+++ test/test_tests.py
@@ -0,0 +1,6 @@
+import pytest
+
+import meowth
+
+def test_true():
+    assert True, True
--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-
-import pytest
-
-class TestSomething(unittest.TestCase):
-    def test_true(self):
-        assert True, True
